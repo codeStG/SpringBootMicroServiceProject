fdfae9a863b88bc87fc4c2da6dbae612de217f75
George <getorres@teksystems.com>
2022-10-10 09:20:34 -0500

    Removed @PostConstruct methods from Dao Implementations

--- src/main/java/com/stgcodes/dao/PersonDaoImpl.java
+++ src/main/java/com/stgcodes/dao/PersonDaoImpl.java
@@ -10,26 +10,4 @@ import javax.annotation.PostConstruct;
 @Component("personDao")
 public class PersonDaoImpl extends DaoImpl<PersonEntity> implements PersonDao {
 
-//    @PostConstruct
-//    public void PostConstruct() {
-//        Person person = Person.builder()
-//                .firstName("Bobbie")
-//                .lastName("Zamudio")
-//                .username("bozamudio")
-//                .dateOfBirth("09/12/2022")
-//                .socialSecurityNumber("123-45-6787")
-//                .gender("female")
-//                .email("bozamudio@onenorth.com")
-//                .build();
-//
-//        PersonEntity personEntity = PersonMapper.INSTANCE.personToPersonEntity(person);
-//
-//        System.out.println("In Post Construct");
-//        this.save(personEntity);
-//        this.findAll().forEach(System.out::println);
-//        System.out.println(this.findById(6L));
-//        this.delete(personEntity);
-//        this.findAll().forEach(System.out::println);
-//        System.out.println("After for each");
-//    }
 }

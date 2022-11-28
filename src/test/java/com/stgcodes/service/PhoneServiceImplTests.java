76c5e69a974b76bac9df27682e40120d5b2069b5
George <stgcodes@gmail.com>
2022-11-28 09:37:14 -0600

    Creating Phone Service tests

---
+++ src/test/java/com/stgcodes/service/PhoneServiceImplTests.java
@@ -0,0 +1,111 @@
+package com.stgcodes.service;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.stgcodes.dao.PersonDao;
+import com.stgcodes.dao.PhoneDao;
+import com.stgcodes.entity.PersonEntity;
+import com.stgcodes.entity.PhoneEntity;
+import com.stgcodes.mappers.PhoneMapper;
+import com.stgcodes.model.Phone;
+import com.stgcodes.validation.PhoneValidator;
+import com.stgcodes.validation.enums.PhoneType;
+
+@ExtendWith(MockitoExtension.class)
+public class PhoneServiceImplTests {
+	
+	@Mock
+	private PhoneDao dao;
+	
+	@Mock
+	private PersonDao personDao;
+	
+	@Mock
+	private PhoneValidator validator;
+	
+	@InjectMocks
+	private PhoneServiceImpl service;
+	
+	Phone testPhone;
+	
+	@BeforeEach
+	void setup() {
+		testPhone = Phone.builder()
+				.phoneNumber("214-214-2142")
+				.phoneType(PhoneType.HOME)
+				.build();
+	}
+	
+	@Test
+	void testFindAll() {
+		when(dao.findAll())
+			.thenReturn(List.of(new PhoneEntity()));
+		
+		service.findAll();
+		
+		verify(dao).findAll();
+	}
+	
+	@Test
+	void testFindById() {		
+		when(dao.findById(1L))
+			.thenReturn(new PhoneEntity());
+		
+		service.findById(1L);
+		
+		verify(dao).findById(1L);
+	}
+	
+//	@Test
+//	void testSave() { 
+//		PhoneEntity phoneEntity = PhoneMapper.INSTANCE.phoneToPhoneEntity(testPhone);
+//		Long personTestId = 1L;
+//		
+////		doReturn(new PersonEntity())
+////			.when(personDao.findById(personTestId));
+//		
+//		when(personDao.findById(personTestId))
+//		.thenReturn(new PersonEntity());
+//		
+//		when(dao.save(phoneEntity))
+//			.thenReturn(new PhoneEntity());
+//		
+//		service.save(testPhone, personTestId);
+//		
+//		verify(dao).save(phoneEntity);
+//	}
+//	
+//	@Test
+//	void testUpdate() {
+//		testPhone.setPhoneId(1L);
+//		
+//		PhoneEntity PhoneEntity = PhoneMapper.INSTANCE.phoneToPhoneEntity(testPhone);
+//		
+//		when(dao.update(PhoneEntity))
+//			.thenReturn(new PhoneEntity());
+//		
+//		service.update(testPhone, 1L);
+//	
+//		verify(dao).update(PhoneEntity);
+//	}
+//	
+//	@Test
+//	void testDelete() {
+//        when(dao.findById(1L))
+//        		.thenReturn(new PhoneEntity());
+//        
+//        service.delete(1L);
+//		
+//		verify(dao).delete(dao.findById(1L));
+//	}
+}	
